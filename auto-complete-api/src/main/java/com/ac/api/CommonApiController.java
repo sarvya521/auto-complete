9ac7fb5557620ae1a54b55541d17145594b4cd35
sarvya521 <Sarvesh.Padwal@experian.com>
2019-02-11 13:59:53 +0800

    cors-cross-origin-added

--- auto-complete-api/src/main/java/com/ac/api/CommonApiController.java
+++ auto-complete-api/src/main/java/com/ac/api/CommonApiController.java
@@ -5,10 +5,7 @@ import java.util.List;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import com.ac.svc.impl.AutoCompleteSvcFacade;
 import com.ac.util.LoggerUtilities;
@@ -43,6 +40,7 @@ public class CommonApiController {
 	 */
 	@SuppressWarnings("rawtypes")
 	@GetMapping("/api/search/{type}")
+	@CrossOrigin(origins = "http://localhost:4200")
 	public List search(@PathVariable(value = "type") String type, @RequestParam(value = "start") String key,
 			@RequestParam(value = "atmost", required = false) Integer maxResult) {
 		LOGGER.info(LoggerUtilities.getMessage("serving auto-complete request for {} with keyword {} and limit {}", type, key, maxResult));
